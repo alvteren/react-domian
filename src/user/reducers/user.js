872c63c2a392d40702d13784c3a6ba26c1c3e17d
Aleksandr Terentev <alvteren@gmail.com>
2018-02-21 14:03:21 +0300

    [bug] after joining to alliance does not update state of user

--- src/user/reducers/user.js
+++ src/user/reducers/user.js
@@ -1,14 +1,18 @@
 const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "USER_AUTHORIZE_SUCCESS":
-    case "USER_CURRENT_GET_SUCCESS":
-      const newValues = {
-        ...payload
-      };
-      return { ...state, ...newValues };
-    default:
-      return state;
+  if (type === "ALIANCE_JOIN_SEND_COMPLETE") {
+    return { ...state, aliance_joined: true };
   }
+  if (
+    type === "USER_AUTHORIZE_SUCCESS" ||
+    type === "USER_CURRENT_GET_SUCCESS"
+  ) {
+    const newValues = {
+      ...payload
+    };
+    return { ...state, ...newValues };
+  }
+
+  return state;
 };
