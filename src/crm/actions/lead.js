ee514b610a46ee968bcef99c82ea5a2ec7b28a5a
Aleksandr Terentev <alvteren@gmail.com>
2018-05-18 11:13:44 +0300

    Started new union api for CRM and refactoring relation entities

--- src/crm/actions/lead.js
+++ src/crm/actions/lead.js
@@ -1,7 +1,9 @@
-import { fetchLeads as fetchLeadsApi } from "../../api/lead";
-import { fetchLead as fetchLeadApi } from "../../api/lead";
-import { fetchLeadFields as fetchLeadFieldsApi } from "../../api/lead";
-import { fetchLeadField as fetchLeadFieldApi } from "../../api/lead";
+import {
+  fetchList,
+  fetchDetail,
+  fetchFields,
+  deleteElement
+} from "../../api/crm";
 import convert from "../../util/leadDataConverter";
 import { setTypeByID } from "../Field/District/districtTreeConverter";
 
@@ -12,9 +14,10 @@ export const fetchLeads = props => async dispatch => {
   try {
     dispatch({
       type: "TABLE_FETCH_DATA_START",
-      payload: { id: "lead" }
+      payload: { entityId: "lead" }
     });
-    const data = await fetchLeadsApi({
+    const data = await fetchList({
+      entityId: "lead",
       filter,
       limit: [page * rowsPerPage, rowsPerPage],
       orderBy,
@@ -23,7 +26,7 @@ export const fetchLeads = props => async dispatch => {
     convert(data);
     dispatch({
       type: "TABLE_FETCH_DATA_SUCCESS",
-      payload: { id: "leads", ...data }
+      payload: { entityId: "lead", ...data }
     });
   } catch (err) {
     dispatch({ type: "TABLE_FETCH_DATA_ERROR", payload: err, error: true });
@@ -34,22 +37,22 @@ export const fetchLead = id => async dispatch => {
   try {
     dispatch({
       type: "DETAIL_FETCH_DATA_START",
-      payload: { id: "leads" }
+      payload: { entityId: "lead" }
     });
-    const data = await fetchLeadApi({ id });
+    const data = await fetchDetail({ id });
     dispatch({
       type: "DETAIL_FETCH_DATA_SUCCESS",
-      payload: { id: "leads", ...data }
+      payload: { entityId: "lead", ...data }
     });
   } catch (err) {
     dispatch({ type: "DETAIL_FETCH_DATA_ERROR", payload: err, error: true });
   }
 };
 export const onInitLead = props => async dispatch => {
-  const { id } = props;
+  const { elementId } = props;
   dispatch({
     type: "DETAIL_INIT",
-    payload: { id: "leads", current: id }
+    payload: { entityId: "lead", current: elementId }
   });
 };
 
@@ -57,33 +60,15 @@ export const fetchLeadFields = () => async dispatch => {
   try {
     dispatch({
       type: "FORM_FIELDS_FETCH_START",
-      payload: { id: "leads" }
+      payload: { id: "lead" }
     });
-    const data = await fetchLeadFieldsApi();
+    const data = await fetchFields({ entityId: "lead" });
     setTypeByID(data);
     dispatch({
       type: "FORM_FIELDS_FETCH_SUCCESS",
-      payload: { id: "leads", data }
+      payload: { id: "lead", data }
     });
   } catch (err) {
     dispatch({ type: "FORM_FIELDS_FETCH_ERROR", payload: err, error: true });
-  }
-};
-export const getField = props => async dispatch => {
-  const { id } = props;
-  try {
-    dispatch({
-      type: "OBJECTS_GET_FIELD_START",
-      payload: { id }
-    });
-    const data = await fetchLeadFieldApi({
-      id
-    });
-    dispatch({
-      type: "OBJECTS_GET_FIELD_SUCCESS",
-      payload: { id, ...data }
-    });
-  } catch (err) {
-    dispatch({ type: "OBJECTS_GET_FIELD_ERROR", payload: err, error: true });
   }
 };
