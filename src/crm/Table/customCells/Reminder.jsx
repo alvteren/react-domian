e78d08c71b6727f9e855d11a61ca6e066dbacee2
laplandin <deniskakhv@gmail.com>
2018-06-02 12:53:55 +0300

    UI for remove action icon

--- src/crm/Table/customCells/Reminder.jsx
+++ src/crm/Table/customCells/Reminder.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { Typography, IconButton, Tooltip, ListItem, ListItemAvatar, ListItemSecondaryAction, Avatar, ListItemText, Divider, List, CardActions } from "material-ui";
+import { Popover, Button, Typography, IconButton, Tooltip, ListItem, ListItemAvatar, ListItemSecondaryAction, Avatar, ListItemText, Divider, List, CardActions } from "material-ui";
 import { AlarmOff as AlarmOffIcon , AlarmAdd as AddAlarmIcon, PhoneForwarded as CallIcon, Person as MeetIcon } from "material-ui-icons";
 import {dateToString} from "../../../util/dateConverter";
 import styles from "./Reminder.module.css";
@@ -18,25 +18,40 @@ class Reminder extends React.PureComponent {
     const isOutdated = new Date(date) - new Date() < 0;
     this.state = {
       showControls: false,
+      anchorEl: null,
       isOutdated
     }
   }
 
 
-  reminderEdit(e) {
+  reminderEdit = (e) => {
   const { elementId, reminderId } = this.props;
     e.preventDefault();
     this.props.history.push(`lead/${elementId}/reminder/${reminderId}`);
+  };
+
+  onRemove = (e) => {
+    e.stopPropagation();
+    this.setState({ anchorEl: e.currentTarget })
+  };
+
+  handleRemove(e) {
+    e.stopPropagation();
+    this.setState({ anchorEl: null })
   }
+
+  handleClose = (e) => {
+    e.stopPropagation();
+    this.setState({ anchorEl: null })
+  };
 
   render() {
     const { reminder, elementId } = this.props;
     return (
       <Fragment>
-        <div className={styles.reminderWrapper}>
+        <div className={this.state.anchorEl ? `${styles.reminderWrapper} ${styles.hover}` : styles.reminderWrapper}>
           <ListItem
             onClick={this.reminderEdit}
-
           >
             <Avatar className={this.state.isOutdated ? styles.reminderAvatarOutdated : styles.reminderAvatar}>
               {
@@ -47,15 +62,32 @@ class Reminder extends React.PureComponent {
             </Avatar>
             <ListItemText
               disableTypography
-              primary={<Typography type="Subheading">reminder.subject</Typography>}
+              primary={<Typography type="Subheading">{reminder.subject}</Typography>}
               secondary={(dateToString(reminder.date))}
             />
             <ListItemSecondaryAction className={styles.reminderActions}>
-              <Tooltip title="Удалить напоминание" enterDelay={300}>
-                <IconButton color="primary" component="span">
-                  <AlarmOffIcon />
-                </IconButton>
-              </Tooltip>
+             <Tooltip title="Удалить напоминание" enterDelay={300}>
+               <IconButton onClick={this.onRemove} color="primary" component="span">
+                 <AlarmOffIcon />
+               </IconButton>
+             </Tooltip>
+              <Popover
+                open={Boolean(this.state.anchorEl)}
+                anchorEl={this.state.anchorEl}
+                onClose={this.handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}
+              >
+                <Typography className={styles.popoverTitle} type="title" align="center" >Вы уверены?</Typography>
+                <Button className={styles.popoverBtn} onClick={this.handleRemove} variant="raised">Да</Button>
+                <Button className={styles.popoverBtn} onClick={this.handleClose} variant="raised">Отмена</Button>
+              </Popover>
               <Tooltip title="Новое напоминание" enterDelay={300}>
                 <Link onClick={(e) => {e.stopPropagation()} } to={`lead/${elementId}/reminder/add`}>
                   <IconButton color="primary" component="span">
